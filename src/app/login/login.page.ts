5b7a51d52d413949a75e993ef5aed222e1d8abb1
Joss-S <20233l001057@utcv.edu.mx>
2025-01-23 09:00:05 -0600

    Implementar formularios

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -17,18 +18,40 @@ export class LoginPage implements OnInit {
 
   ngOnInit() {
   }
-  login(){
-    // Si usuario = admin y password = admin
-    //Entonces console.log('Login correcto)
-    //Sino console.log('Login incorrecto')
-  if (
-     this.email == 'admin' &&
-     this.password =='admin'){
-     console.log('Login correcto');
-     }
-     else{
-      console.log('Login incorrecto')
-     }
+  login( tangamandapio: NgForm){
+    if(tangamandapio.invalid){
+      console.log('Todos los campos requeridos');
+      return;
     }
-//verificación con precision se le pone === y si no importa tanto solo se ponen dos ==
+    console.log('email: ', this.email);
+    console.log('password: ', this.password);
+    //Obtenemos los valores del formulario
+    console.log(tangamandapio.value);
+
+    //Validar formulario
+    console.log('valid', tangamandapio.valid);
+
+
+    //Si tangamandapio es invalido
+    //console.log('Todos los campos requeridos')
+    //return
+    if(
+      tangamandapio.valid ==true
+    
+    ){    // Si usuario = admin y password = admin
+      //Entonces console.log('Login correcto)
+      //Sino console.log('Login incorrecto')
+    if (
+       this.email == 'admin' &&
+       this.password =='admin'){
+       console.log('Login correcto');
+       }
+       else{
+        console.log('Login incorrecto');
+       }
+      }
+     
+  //verificación con precision se le pone === y si no importa tanto solo se ponen dos ==}else
+
+    }
 }
