d2e4ff62fc346772364acd3c02c8e0bec7395ff7
Joss-S <20233l001057@utcv.edu.mx>
2025-01-23 08:12:42 -0600

    Enlazar login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
+
+  constructor() {
+ 
+   }
 
   ngOnInit() {
   }
-
+  login(){
+    // Si usuario = admin y password = admin
+    //Entonces console.log('Login correcto)
+    //Sino console.log('Login incorrecto')
+  if (
+     this.email == 'admin' &&
+     this.password =='admin'){
+     console.log('Login correcto');
+     }
+     else{
+      console.log('Login incorrecto')
+     }
+    }
+//verificación con precision se le pone === y si no importa tanto solo se ponen dos ==
 }
